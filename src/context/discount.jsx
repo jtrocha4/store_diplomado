96e02f1654cca585724584d5db2bbf449105b557
jtrocha4 <jtrocha04@gmail.com>
2024-09-19 21:36:24 -0500

    update(Context): discount

--- src/context/discount.jsx
+++ src/context/discount.jsx
@@ -2,13 +2,15 @@ import { createContext, useState } from 'react'
 
 export const DiscountContext = createContext()
 
-export const DiscountProvider = () => {
+export const DiscountProvider = ({ children }) => {
   const [discount, setDiscount] = useState(0)
   return (
     <DiscountContext.Provider value={{
       discount,
       setDiscount
     }}
-    />
+    >
+      {children}
+    </DiscountContext.Provider>
   )
 }
